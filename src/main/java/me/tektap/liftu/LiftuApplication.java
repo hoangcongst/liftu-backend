ed74c17a08ec29d941c853519acebd118163262c
money <you@example.com>
2020-08-09 21:57:22 +0700

    init project

---
+++ src/main/java/me/tektap/liftu/LiftuApplication.java
@@ -0,0 +1,13 @@
+package me.tektap.liftu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiftuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiftuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/tektap/liftu/LiftuApplicationTests.java
@@ -0,0 +1,13 @@
+package me.tektap.liftu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiftuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
