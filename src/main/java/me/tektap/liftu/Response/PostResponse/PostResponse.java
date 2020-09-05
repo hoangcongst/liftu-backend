dd804f4b75370659e13777a5e39ea9c3421f66ed
conght <conght@funtap.vn>
2020-09-05 16:51:18 +0700

    update api post

--- src/main/java/me/tektap/liftu/Response/PostResponse/PostResponse.java
+++ src/main/java/me/tektap/liftu/Response/PostResponse/PostResponse.java
@@ -1,5 +1,6 @@
 package me.tektap.liftu.Response.PostResponse;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import me.tektap.liftu.Response.BaseResponse;
 import me.tektap.liftu.entity.Post.Post;
@@ -7,6 +8,7 @@ import me.tektap.liftu.entity.Post.Post;
 public class PostResponse extends BaseResponse {
 
     @Getter
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private final Post data;
 
     public PostResponse(int status, Post mPost) {
